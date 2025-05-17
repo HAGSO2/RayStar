194f120fa9db1b07f637f29bd7b8f6b1d33a7034
HAGSO2 <83025616+HAGSO2@users.noreply.github.com>
2025-05-17 11:43:42 +0200

    Calcula vecinos

--- Abstractos/AStar.cpp
+++ Abstractos/AStar.cpp
@@ -27,10 +27,77 @@ void AStar::Step(){
     CalcNeightbours();
 }
 
+/*  
+    *******
+    *O O O*
+    *^ ^ ^*
+    *` | ´*
+    *O<C>O*
+    *´ | `*
+    *v v v*
+    *O O O*
+    *******
+*/
+
 void AStar::CalcNeightbours(){
-    if((currentPosition.x > 0 && currentPosition.y > 0 && currentPosition.x < CELL_X && currentPosition.y < CELL_Y) &&
-    tabletop[(int)currentPosition.x][(int)currentPosition.y].type != WALL ||
-    tabletop[(int)currentPosition.x][(int)currentPosition.y].type != HARDWALL){
-        open.push_back(tabletop[(int)currentPosition.x][(int)currentPosition.y]);
+    // if((currentPosition.x > 0 && currentPosition.y > 0 && currentPosition.x < CELL_X-1 && currentPosition.y < CELL_Y-1) &&
+    // tabletop[(int)currentPosition.x][(int)currentPosition.y].type != WALL ||
+    // tabletop[(int)currentPosition.x][(int)currentPosition.y].type != HARDWALL){
+    //     open.push_back(tabletop[(int)currentPosition.x][(int)currentPosition.y]);
+    // }
+    //O
+    //`C
+    if((currentPosition.x > 0 && currentPosition.y > 0) &&
+    tabletop[(int)currentPosition.x-1][(int)currentPosition.y-1].type != WALL ||
+    tabletop[(int)currentPosition.x-1][(int)currentPosition.y-1].type != HARDWALL){
+        open.push_back(tabletop[(int)currentPosition.x-1][(int)currentPosition.y-1]);
+    }
+    //O
+    //ĉ
+    if((currentPosition.y > 0 ) &&
+    tabletop[(int)currentPosition.x][(int)currentPosition.y-1].type != WALL ||
+    tabletop[(int)currentPosition.x][(int)currentPosition.y-1].type != HARDWALL){
+        open.push_back(tabletop[(int)currentPosition.x][(int)currentPosition.y-1]);
+    }
+    // O
+    //Ć
+    if((currentPosition.y > 0 && currentPosition.x < CELL_X-1) &&
+    tabletop[(int)currentPosition.x+1][(int)currentPosition.y-1].type != WALL ||
+    tabletop[(int)currentPosition.x+1][(int)currentPosition.y-1].type != HARDWALL){
+        open.push_back(tabletop[(int)currentPosition.x+1][(int)currentPosition.y-1]);
+    }
+    //O < C
+    if((currentPosition.x > 0) &&
+    tabletop[(int)currentPosition.x-1][(int)currentPosition.y].type != WALL ||
+    tabletop[(int)currentPosition.x-1][(int)currentPosition.y].type != HARDWALL){
+        open.push_back(tabletop[(int)currentPosition.x-1][(int)currentPosition.y]);
+    }
+    //C > O
+    if((currentPosition.x < CELL_X-1) &&
+    tabletop[(int)currentPosition.x+1][(int)currentPosition.y].type != WALL ||
+    tabletop[(int)currentPosition.x+1][(int)currentPosition.y].type != HARDWALL){
+        open.push_back(tabletop[(int)currentPosition.x+1][(int)currentPosition.y]);
+    }
+    // C
+    //Ó
+    if((currentPosition.x > 0 && currentPosition.y < CELL_Y-1) &&
+    tabletop[(int)currentPosition.x-1][(int)currentPosition.y+1].type != WALL ||
+    tabletop[(int)currentPosition.x-1][(int)currentPosition.y+1].type != HARDWALL){
+        open.push_back(tabletop[(int)currentPosition.x-1][(int)currentPosition.y+1]);
+    }
+    //C
+    //V
+    //O
+    if((currentPosition.y < CELL_Y-1) &&
+    tabletop[(int)currentPosition.x][(int)currentPosition.y+1].type != WALL ||
+    tabletop[(int)currentPosition.x][(int)currentPosition.y+1].type != HARDWALL){
+        open.push_back(tabletop[(int)currentPosition.x][(int)currentPosition.y+1]);
+    }
+    //C
+    // Ò
+    if((currentPosition.x < CELL_X-1 && currentPosition.y < CELL_Y-1) &&
+    tabletop[(int)currentPosition.x+1][(int)currentPosition.y+1].type != WALL ||
+    tabletop[(int)currentPosition.x+1][(int)currentPosition.y+1].type != HARDWALL){
+        open.push_back(tabletop[(int)currentPosition.x+1][(int)currentPosition.y+1]);
     }
 }
